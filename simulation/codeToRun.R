ee147c6131f84a6b2bfd3087caa2fd47bc927c15
catalamarti <marticatalasabate@gmail.com>
2026-07-06 14:18:49 +0100

    Update codeToRun.R

--- simulation/codeToRun.R
+++ simulation/codeToRun.R
@@ -11,13 +11,14 @@ library(readr)
 sampleSize <- c(2, 5) # log
 treatmentPrevalence <- c(0.01, 0.99) # lin
 numberCovariates <- c(2, 3) # log
-unmeasured <- c(-2, 0) # log
+unmeasured <- c(-10, -8) # log
 outcomePrevalence <- c(-3, -1) # log
 treatmentEffect <- c(-1, 1) # lin
 numberNCO <- c(0, 3) # log
+covariateSd <- c(0, 0.3) # lin
 
-covariateSd <- 0.75
-numberSimulations <- 3
+numberSimulations <- 10
+minimumTreatmentPerClassForLasso <- 4
 
 rand <- function(x, range) {
   range[1] + (range[2] - range[1]) * x
@@ -26,7 +27,7 @@ rand10 <- function(x, range) {
   10 ** (range[1] + (range[2] - range[1]) * x)
 }
 
-parameters <- randomLHS(numberSimulations, 7) 
+parameters <- randomLHS(numberSimulations, 8)
 
 parameters <- tibble(
   sample_size = round(rand10(parameters[,1], sampleSize)),
@@ -35,7 +36,8 @@ parameters <- tibble(
   unmeasured = round(rand10(parameters[,4], unmeasured) * number_covariates),
   outcome_prevalence = rand10(parameters[,5], outcomePrevalence),
   treatment_effect = rand(parameters[,6], treatmentEffect),
-  nco = round(rand10(parameters[,7], numberNCO))
+  nco = round(rand10(parameters[,7], numberNCO)),
+  covariate_sd = rand(parameters[,8], covariateSd),
 ) |>
   filter(sample_size > 2 * number_covariates)
 
@@ -43,10 +45,10 @@ fmt <- paste0("cov_%0", ceiling(numberCovariates[2]),"i")
 fmtnco <- paste0("nco_%0", ceiling(numberCovariates[2]),"i")
 
 result <- parameters |>
-  pmap(\(sample_size, treatment_prevalence, number_covariates, unmeasured, outcome_prevalence, treatment_effect, nco) {
+  pmap(\(sample_size, treatment_prevalence, number_covariates, unmeasured, outcome_prevalence, treatment_effect, nco, covariate_sd) {
     ts <- Sys.time()
     n <- sample_size
-    
+
     # result
     result <- tibble(
       sample_size = n,
@@ -55,40 +57,52 @@ result <- parameters |>
       unmeasured = unmeasured,
       outcome_prevalence = outcome_prevalence,
       treatment_effect = treatment_effect,
-      nco = nco
+      nco = nco,
+      covariate_sd = covariate_sd
     )
-    
+
     # covariates
     covariates <- rep(0.2, number_covariates) |>
       imap(\(p, i) {
         tibble(!!sprintf(fmt, i) := if_else(runif(n = n) < p, 1, 0))
       }) |>
       bind_cols()
-    
+
     # covariates -> treatment
-    cov <- rnorm(n = number_covariates, sd = covariateSd)
-    
+    cov <- rnorm(n = number_covariates, sd = covariate_sd)
+
     # treatment
     p_treatment <- as.vector(as.matrix(covariates) %*% cov)
     fn <- \(x) mean(1 / (1 + exp(-(x + p_treatment)))) - treatment_prevalence
     delta <- uniroot(fn, interval = c(-100, 100))$root
     p_treatment <- 1 / (1 + exp(-(delta + p_treatment)))
     treatment <- if_else(runif(n = n) < p_treatment, 1, 0)
-    
-    if (length(unique(treatment)) != 2) {
+
+    treatment_class_count <- table(treatment)
+    if (length(treatment_class_count) != 2) {
       return(NULL)
     }
-    
+
     # covariates -> outcome
-    cov <- rnorm(n = number_covariates, sd = covariateSd)
-    
+    cov <- rnorm(n = number_covariates, sd = covariate_sd)
+
     # outcome
-    p_outcome <- as.vector(as.matrix(covariates) %*% cov) + treatment_effect * treatment
-    fn <- \(x) mean(1 / (1 + exp(-(x + p_outcome)))) - outcome_prevalence
+    linear_outcome <- as.vector(as.matrix(covariates) %*% cov)
+    linear_outcome_observed <- linear_outcome + treatment_effect * treatment
+    fn <- \(x) mean(1 / (1 + exp(-(x + linear_outcome_observed)))) - outcome_prevalence
     delta <- uniroot(fn, interval = c(-100, 100))$root
-    p_outcome <- 1 / (1 + exp(-(delta + p_outcome)))
+    p_outcome_0 <- 1 / (1 + exp(-(delta + linear_outcome)))
+    p_outcome_1 <- 1 / (1 + exp(-(delta + linear_outcome + treatment_effect)))
+    p_outcome <- if_else(treatment == 1, p_outcome_1, p_outcome_0)
     outcome <- if_else(runif(n = n) < p_outcome, 1, 0)
-    
+
+    marginal_treatment_effect <- qlogis(mean(p_outcome_1)) - qlogis(mean(p_outcome_0))
+    overlap_target_weight <- p_treatment * (1 - p_treatment)
+    overlap_treatment_effect <- qlogis(weighted.mean(p_outcome_1, overlap_target_weight)) -
+      qlogis(weighted.mean(p_outcome_0, overlap_target_weight))
+    result$marginal_treatment_effect <- marginal_treatment_effect
+    result$overlap_treatment_effect <- overlap_treatment_effect
+
     # negative control outcomes
     p_nco <- as.vector(as.matrix(covariates) %*% cov)
     nco <- 10 ** runif(n = nco, min = -3, max = -0.5) |>
@@ -99,26 +113,27 @@ result <- parameters |>
         tibble(!!sprintf(fmtnco, i) := if_else(runif(n = n) < p_nco, 1, 0))
       }) |>
       bind_cols()
-    
+
     # remove unmeasured confounding
     covariates <- covariates |>
       select(!seq_len(unmeasured))
-    
+
     # crude coefficient
     x <- tibble(outcome = outcome, treatment = treatment)
     crude <- glm(outcome ~ treatment, data = x, family = binomial(link = "logit")) |>
       tidy() |>
       filter(term == "treatment")
-    result$crude_error <- crude$estimate - treatment_effect
+    result$crude_error <- crude$estimate - marginal_treatment_effect
     result$crude_sd <- crude$std.error
-    
+
     # lasso regression
-    if (ncol(covariates) > 0) {
+    if (ncol(covariates) > 0 && min(treatment_class_count) >= minimumTreatmentPerClassForLasso) {
       cv_fit <- cv.glmnet(
         x = as.matrix(covariates),
         y = treatment,
         family = "binomial",
-        alpha = 1
+        alpha = 1,
+        nfolds = min(10, min(treatment_class_count))
       )
       lasso_coef <- coef(cv_fit, s = "lambda.min")
       covs <- rownames(lasso_coef)[as.vector(lasso_coef != 0)]
@@ -126,7 +141,7 @@ result <- parameters |>
     } else {
       covs <- character()
     }
-    
+
     # weights
     if (length(covs) > 0) {
       formula <- paste0("treatment ~ ", paste0(covs, collapse = " + "))
@@ -135,19 +150,22 @@ result <- parameters |>
     } else {
       prob <- mean(treatment)
     }
+    
+    fit |> tidy() |> pull("estimate") |> sd()
+    
     x <- x |>
       mutate(
         ps = prob,
         weight = if_else(treatment == 1, 1 - ps, ps)
       )
-    
+
     # weighted coefficient
     weighted <- glm(outcome ~ treatment, data = x, weights = weight, family = binomial(link = "logit")) |>
       tidy() |>
       filter(term == "treatment")
-    result$weighted_error <- weighted$estimate - treatment_effect
+    result$weighted_error <- weighted$estimate - overlap_treatment_effect
     result$weighted_sd <- weighted$std.error
-    
+
     # negative control outcomes
     ncos <- nco |>
       map(\(out) {
@@ -164,9 +182,9 @@ result <- parameters |>
     result$nco_model_sd_intercept <- model[3]
     result$nco_model_sd_slope <- model[4]
     calibrated <- calibrateConfidenceInterval(weighted$estimate, weighted$std.error, model)
-    result$calibrated_error <- calibrated$logRr - treatment_effect
+    result$calibrated_error <- calibrated$logRr - overlap_treatment_effect
     result$calibrated_sd <- calibrated$seLogRr
-    
+
     # adjusted model
     x <- x |>
       bind_cols(nco)
@@ -174,21 +192,21 @@ result <- parameters |>
     crude_adjusted <- glm(formula = formula, data = x, family = binomial(link = "logit")) |>
       tidy() |>
       filter(term == "treatment")
-    result$crude_adjusted_error <- crude_adjusted$estimate - treatment_effect
+    result$crude_adjusted_error <- crude_adjusted$estimate - marginal_treatment_effect
     result$crude_adjusted_sd <- crude_adjusted$std.error
     weighted_adjusted <- glm(formula = formula, data = x, weights = weight, family = binomial(link = "logit")) |>
       tidy() |>
       filter(term == "treatment")
-    result$weighted_adjusted_error <- weighted_adjusted$estimate - treatment_effect
+    result$weighted_adjusted_error <- weighted_adjusted$estimate - overlap_treatment_effect
     result$weighted_adjusted_sd <- weighted_adjusted$std.error
-    
+
     # measured
     result$measured_treatment_prevalence <- mean(x$treatment)
     result$measured_outcome_prevalence <- mean(x$outcome)
-    
+
     # time
     result$time <- as.numeric(difftime(time1 = Sys.time(), time2 = ts))
-    
+
     return(result)
   }) |>
   bind_rows()
